8ed7d559fa1a14d4f3dfcb081f54870b909d956c
Colby <colby@auscert.org.au>
2019-06-04 15:14:00 +1000

    fix up athena queries and misp dump

--- misp_dump_attributes.py
+++ misp_dump_attributes.py
@@ -8,8 +8,8 @@ misp = PyMISP('https://misp.localhost/', '<api-key>', True, 'json')
 ret = ""
 result = misp.search('attributes', type_attribute = 'filename', to_ids = True)
 for attribute in result['response']['Attribute']:
-    ret += attribute['id'] + ", "
-    ret += attribute['event_id'] + ", "
+    ret += attribute['id'] + ","
+    ret += attribute['event_id'] + ","
     ret += attribute['value'] + "\n"
 
 print (ret)
