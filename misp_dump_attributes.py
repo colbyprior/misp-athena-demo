a12696c716b13b75112fb715a9a12c80d2febae5
Colby Prior <cheesecolby7@gmail.com>
2019-06-13 09:22:02 +1000

    Update misp_dump_attributes.py
    change type for example link

--- misp_dump_attributes.py
+++ misp_dump_attributes.py
@@ -6,7 +6,7 @@ import json
 misp = PyMISP('https://misp.localhost/', '<api-key>', True, 'json')
 
 ret = ""
-result = misp.search('attributes', type_attribute = 'filename', to_ids = True)
+result = misp.search('attributes', type_attribute = 'ip-dst', to_ids = True)
 for attribute in result['response']['Attribute']:
     ret += attribute['id'] + ","
     ret += attribute['event_id'] + ","
